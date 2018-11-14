51d3212dae39a16ec02103e87b2441fee7c8ceba
christophe12 <mutachristophe12@gmail.com>
2018-11-14 18:11:22 +0200

    third version one commit

--- src/index.js
+++ src/index.js
@@ -6,16 +6,18 @@ import SingleCustomerInvoice from './SingleCustomerInvoice';
 class InvoiceShow extends Component {
     render() {
         const { invoicesList, onLoad } = this.props;
-        <div className="row no-gutters indexView">
-            {/* ********** Invoices Quick View List ************ */}
-            <div className="col-md-4 col-sm-4 col-xs-12">
-                <DeliveryNotesList invoicesData={invoicesList} loader={onLoad} />
+        return (
+            <div>
+                {/* ********** Invoices Quick View List ************ */}
+                <div className="col-md-4 col-sm-4 col-xs-12">
+                    <DeliveryNotesList invoicesData={invoicesList} loader={onLoad} />
+                </div>
+                {/* ********** Invoice Wrapper ************ */}
+                <div className="col-md-8 col-sm-8 col-xs-12 invoiceWrapper">
+                    <SingleCustomerInvoice invoicesData={invoicesList} loader={onLoad} />
+                </div>
             </div>
-            {/* ********** Invoice Wrapper ************ */}
-            <div className="col-md-8 col-sm-8 col-xs-12 invoiceWrapper">
-                <SingleCustomerInvoice invoicesData={invoicesList} loader={onLoad} />
-            </div>
-        </div>
+        );
     }
 };
 
